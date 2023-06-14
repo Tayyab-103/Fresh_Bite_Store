8b9d695b3970b7306e13020546bbcda9eb147ba0
Tayyab-103 <tayyabhameed103@gmail.com>
2023-06-14 16:38:48 +0500

    some More changes

--- frontend/src/page/About.jsx
+++ frontend/src/page/About.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const About = () => {
   const Navigate = useNavigate();
   const routeChange = () => {
-    Navigate("/home");
+    Navigate("/");
   };
   return (
     <>
@@ -24,22 +24,22 @@ const About = () => {
       <h2 class="text-3xl font-bold mb-4">Add more content about your online food store here</h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <div class="bg-white rounded-lg shadow-lg p-4">
-          <img onClick={routeChange} src="https://www.thestar.com/content/dam/thestar/life/food_wine/2017/01/15/fresh-bites-new-apple-brand-pazazz-available-now-in-stores/pazazz-in-apple-basket.jpg" alt="Team Member 1" class="w-full mb-4 rounded-lg"/>
+          <img onClick={routeChange} src="https://www.thestar.com/content/dam/thestar/life/food_wine/2017/01/15/fresh-bites-new-apple-brand-pazazz-available-now-in-stores/pazazz-in-apple-basket.jpg" alt="Team Member 1" class="w-full mb-4 rounded-lg cursor-pointer"/>
           <h3 class="text-lg font-semibold">Fresh Apples</h3>
           <p class="text-gray-700"> Red Delicious is our choice for best apple for overall nutrition</p>
         </div>
         <div class="bg-white rounded-lg shadow-lg p-4">
-          <img onClick={routeChange} src="https://cdn-prod.medicalnewstoday.com/content/images/articles/271/271157/bananas-chopped-up-in-a-bowl.jpg" alt="Team Member 2" class="w-full mb-4 rounded-lg"/>
+          <img onClick={routeChange} src="https://cdn-prod.medicalnewstoday.com/content/images/articles/271/271157/bananas-chopped-up-in-a-bowl.jpg" alt="Team Member 2" class="w-full mb-4 rounded-lg cursor-pointer"/>
           <h3 class="text-lg font-semibold">Fresh Bananas</h3>
           <p class="text-gray-700">Bananas contain fiber, potassium, folate, and antioxidants, such as vitamin C.</p>
         </div>
         <div class="bg-white rounded-lg shadow-lg p-4">
-          <img onClick={routeChange} src="https://as2.ftcdn.net/v2/jpg/05/64/04/73/1000_F_564047323_IPjf48i1GXM01eLGuI1pAD8EzWHl4S8V.jpg" alt="Team Member 3" class="w-full mb-4 rounded-lg"/>
+          <img onClick={routeChange} src="https://as2.ftcdn.net/v2/jpg/05/64/04/73/1000_F_564047323_IPjf48i1GXM01eLGuI1pAD8EzWHl4S8V.jpg" alt="Team Member 3" class="w-full mb-4 rounded-lg cursor-pointer"/>
           <h3 class="text-lg font-semibold">Fresh Vegetables</h3>
           <p class="text-gray-700">Organic, chemical-free and farmer-friendly</p>
         </div>
         <div class="bg-white rounded-lg shadow-lg p-4">
-          <img onClick={routeChange} src="https://media.istockphoto.com/id/1309966291/photo/delivery-fast-food-concept.jpg?s=612x612&w=0&k=20&c=TcY_bJDPphPUl1X9PtsWXfLBdmRRcd9wbiOHbuN8O9k=" alt="Team Member 4" class="w-full mb-4 rounded-lg"/>
+          <img onClick={routeChange} src="https://media.istockphoto.com/id/1309966291/photo/delivery-fast-food-concept.jpg?s=612x612&w=0&k=20&c=TcY_bJDPphPUl1X9PtsWXfLBdmRRcd9wbiOHbuN8O9k=" alt="Team Member 4" class="w-full mb-4 rounded-lg cursor-pointer"/>
           <h3 class="text-lg font-semibold">Fresh Fast Food</h3>
           <p class="text-gray-700">Delivery fastfood ordering food online concept.</p>
         </div>
