17f1d8cc3900fd635a018e7b617f9306a4e99803
Tayyab-103 <tayyabhameed103@gmail.com>
2023-07-18 02:54:05 +0500

    Email.js Functionality add

--- frontend/src/page/Contact.jsx
+++ frontend/src/page/Contact.jsx
@@ -1,35 +1,49 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const SendMail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_vm11d4j', 'template_skzqm0u', form.current, 'PJjUIOsZmvVi6sE_B')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <>
   
-     <div class="bg-gray-700 text-white py-4 text-center">
-       <div class="container mx-auto px-4">
-         <h1 class="text-2xl font-bold">Fresh-Bite Contact </h1>
+     <div className="bg-gray-700 text-white py-4 text-center">
+       <div className="container mx-auto px-4">
+         <h1 className="text-2xl font-bold">Fresh-Bite Contact </h1>
        </div>
      </div>
 
-     <div class="container mx-auto px-4 py-8">
+     <div className="container mx-auto px-4 py-8">
        <div>
-         <h2 class="text-3xl font-bold mb-4 text-center">Contact Us</h2>
-         <p class="mb-4 text-center">We would love to hear from you! Please provide your information below:</p>
+         <h2 className="text-3xl font-bold mb-4 text-center">Contact Us</h2>
+         <p className="mb-4 text-center">We would love to hear from you! Please provide your information below:</p>
         
-         <form class="max-w-md mx-auto">
-           <div class="mb-4">
-             <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name:</label>
-             <input type="text" id="name" name="name" placeholder="Enter your name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+         <form ref={form} onSubmit={SendMail} className="max-w-md mx-auto">
+           <div className="mb-4">
+             <label for="from_name" className="block text-gray-700 text-sm font-bold mb-2">Name:</label>
+             <input type="text" id="from_name" name="from_name" placeholder="Enter your name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
            </div>
-           <div class="mb-4">
-             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-             <input type="email" id="email" name="email" placeholder="Enter your email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+           <div className="mb-4">
+             <label for="email_id" className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+             <input type="email" id="email_id" name="email_id" placeholder="Enter your email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
            </div>
-           <div class="mb-4">
-             <label for="message" class="block text-gray-700 text-sm font-bold mb-2">Message:</label>
-             <textarea id="message" name="message" placeholder="Enter your message" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+           <div className="mb-4">
+             <label for="message" className="block text-gray-700 text-sm font-bold mb-2">Message:</label>
+             <textarea id="message" name="message" placeholder="Enter your message" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
            </div>
-           <div class="flex justify-center">
-             <button type="submit" class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+           <div className="flex justify-center">
+             <button type="submit" className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                Submit
              </button>
            </div>
@@ -38,9 +52,9 @@ const Contact = () => {
      </div>
 <br/>
 <br/>
-     <footer class="bg-gray-700 text-white py-6 ">
-       <div class="container mx-auto px-4">
-         <p class="text-center">© 2023 Fresh-Bite Store. All rights reserved.</p>
+     <footer className="bg-gray-700 text-white py-6 ">
+       <div className="container mx-auto px-4">
+         <p className="text-center">© 2023 Fresh-Bite Store. All rights reserved.</p>
        </div>
      </footer>
        </>
